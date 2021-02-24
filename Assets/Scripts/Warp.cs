734ebc3bad89510a208011cbd5bb4fa26f3bd9e8
Kniro04 <keenabril@gmail.com>
2021-02-25 00:02:54 +0100

    transition
    transition

---
+++ Assets/Scripts/Warp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class Warp : MonoBehaviour
+{
+    // Para almacenar el punto de destino
+    public GameObject target;
+
+    void Awake ()
+    {
+        // Si queremos podemos esconder el debug de los Warps
+        GetComponent<SpriteRenderer> ().enabled = false;
+        transform.GetChild (0).GetComponent<SpriteRenderer> ().enabled = false;
+    }
+
+    void OnTriggerEnter2D (Collider2D other) {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Actualizamos la posición
+            other.transform.position = target.transform.GetChild (0).transform.position;
+
+        }
+    }
+}
