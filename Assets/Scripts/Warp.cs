e0b6262127c8151e6473c0dc350616eed57af7dd
Kniro04 <keenabril@gmail.com>
2021-04-16 15:43:03 +0200

    Prototipo2

--- Assets/Scripts/Warp.cs
+++ Assets/Scripts/Warp.cs
@@ -8,6 +8,15 @@ public class Warp : MonoBehaviour
     // Para almacenar el punto de destino
     public GameObject target;
 
+    // Para controlar si empieza o no la transición
+    bool start = false;
+    // Para controlar si la transición es de entrada o salida
+    bool isFadeIn = false;
+    // Opacidad inicial del cuadrado de transición
+    float alpha = 0;
+    // Transición de 1 segundo
+    float fadeTime = 1.5f;
+
     void Awake ()
     {
         // Si queremos podemos esconder el debug de los Warps
@@ -15,13 +24,70 @@ public class Warp : MonoBehaviour
         transform.GetChild (0).GetComponent<SpriteRenderer> ().enabled = false;
     }
 
-    void OnTriggerEnter2D (Collider2D other) {
+    IEnumerator OnTriggerEnter2D (Collider2D other) {
 
         if (other.gameObject.CompareTag("Player"))
         {
+            other.GetComponent<Animator>().enabled = false;
+            other.GetComponent<PlayerMovment>().enabled = false;
+            FadeIn();
+
+            yield return new WaitForSeconds(fadeTime);
+
             // Actualizamos la posición
             other.transform.position = target.transform.GetChild (0).transform.position;
 
+            FadeOut();
+            other.GetComponent<Animator>().enabled = true;
+            other.GetComponent<PlayerMovment>().enabled = true;
         }
+    }
+
+    void OnGUI()
+    {
+
+        // Si no empieza la transición salimos del evento directamente
+        if (!start)
+            return;
+
+        // Si ha empezamos creamos un color con una opacidad inicial a 0
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+
+        // Creamos una textura temporal para rellenar la pantalla
+        Texture2D tex;
+        tex = new Texture2D(1, 1);
+        tex.SetPixel(0, 0, Color.black);
+        tex.Apply();
+
+        // Dibujamos la textura sobre toda la pantalla
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), tex);
+
+        // Controlamos la transparencia
+        if (isFadeIn)
+        {
+            // Si es la de aparecer le sumamos opacidad
+            alpha = Mathf.Lerp(alpha, 1.1f, fadeTime * Time.deltaTime);
+        }
+        else
+        {
+            // Si es la de desaparecer le restamos opacidad
+            alpha = Mathf.Lerp(alpha, -0.1f, fadeTime * Time.deltaTime);
+            // Si la opacidad llega a 0 desactivamos la transición
+            if (alpha < 0) start = false;
+        }
+
+    }
+
+    // Método para activar la transición de entrada
+    void FadeIn()
+    {
+        start = true;
+        isFadeIn = true;
+    }
+
+    // Método para activar la transición de salida
+    void FadeOut()
+    {
+        isFadeIn = false;
     }
 }
